04e04f99ff6ae83b2eb1e97f63ff84f7b3e32449
Sébastien Harinck <sebastien.harinck@odaxiom.com>
2020-02-27 11:12:39 +0100

    feat(setup): add the setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+
+setuptools.setup(
+    name="vdata",
+    version="0.0.1",
+    author="Sebastien Harinck",
+    author_email="sebastien.harinck@odaxiom.com",
+    description="A python package that aims to provide an abstraction about the management of any types of Data",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.8',
+)
