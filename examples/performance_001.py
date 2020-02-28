66ca079c997f6764a075a6473fbd39c7b1b9a42d
Sébastien Harinck <sebastien.harinck@odaxiom.com>
2020-02-28 11:07:44 +0100

    docs(examples): add performance example

---
+++ examples/performance_001.py
@@ -0,0 +1,33 @@
+from vdata import Data
+import time
+
+
+N = 2000
+SUM_UPDATE = 0
+SUM_GET = 0
+
+# instantiate a virtual data
+variable = Data(name='variable', namespace='raw')
+
+for i in range(N):
+    t1 = time.time()
+    variable.update(data=i)
+    t2 = time.time()
+
+    delta = t2 - t1
+    SUM_UPDATE += delta
+
+for i in range(N):
+    t1 = time.time()
+    a = variable.get(revision=i)
+    t2 = time.time()
+
+    delta = t2 - t1
+    SUM_GET += delta
+
+
+print(SUM_UPDATE)
+print(SUM_GET)
+
+print(SUM_UPDATE / N)
+print(SUM_GET / N)
